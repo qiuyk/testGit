ea112f005da6cd78fa253d860e4570dbc0546d79
qiuyk <449263856@qq.com>
2018-11-06 22:11:49 +0800

    init

---
+++ src/main/java/com/taotao/test/TestA.java
@@ -0,0 +1,14 @@
+package com.taotao.test;
+
+/**
+ * @Description:
+ * @Author: qiuyk
+ * @Date: 2018-11-06
+ */
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("test git");
+
+        System.out.println("test git2");
+    }
+}
